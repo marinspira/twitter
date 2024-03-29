4453f2a0c32bd80f7d14440a1711389827344997
Maria Eduarda Ferreira <maria.ferreira@feedz.com.br>
2024-03-29 19:24:12 +0000

    Set routes and form signup

---
+++ src/_auth/forms/SigninForm.tsx
@@ -0,0 +1,9 @@
+function SigninForm() {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default SigninForm
---
+++ src/_auth/forms/SignupForm.tsx
@@ -0,0 +1,106 @@
+import { zodResolver } from "@hookform/resolvers/zod"
+import { useForm } from "react-hook-form"
+import {
+  Form,
+  FormControl,
+  FormDescription,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "@/components/ui/form"
+import { Input } from "@/components/ui/input"
+import { Button } from "@/components/ui/button"
+import { SignupValidation } from "@/lib/validation"
+import { z } from "zod"
+
+function SignupForm() {
+
+  const form = useForm<z.infer<typeof SignupValidation>>({
+    resolver: zodResolver(SignupValidation),
+    defaultValues: {
+      name: "",
+      username: "",
+      email: "",
+      password: ""
+    },
+  })
+
+  function onSubmit(values: z.infer<typeof SignupValidation>) {
+    console.log(values)
+  }
+
+  return (
+    <Form {...form}>
+      <div className="sm:w-420 md:w-600 flex-center flex-col">
+        <img src="" />
+
+        <h2 className="h3-bold md:h1-bold pt-5 sm:pt-12">
+          Create a new account
+        </h2>
+        
+        <p className="text-light-3 small-medium md:base-regular mt-2">
+          Enter your datails to use
+        </p>
+
+        <form onSubmit={form.handleSubmit(onSubmit)} className="flex flex-col gap-5 w-full mt-4">
+          <FormField
+            control={form.control}
+            name="name"
+            render={({ field }) => (
+              <FormItem>
+                <FormLabel>Name</FormLabel>
+                <FormControl>
+                  <Input type="text" className="shad-input" {...field} />
+                </FormControl>
+                <FormMessage />
+              </FormItem>
+            )}
+          />
+          <FormField
+            control={form.control}
+            name="username"
+            render={({ field }) => (
+              <FormItem>
+                <FormLabel>Username</FormLabel>
+                <FormControl>
+                  <Input type="text" className="shad-input" {...field} />
+                </FormControl>
+                <FormMessage />
+              </FormItem>
+            )}
+          />
+          <FormField
+            control={form.control}
+            name="email"
+            render={({ field }) => (
+              <FormItem>
+                <FormLabel>E-mail</FormLabel>
+                <FormControl>
+                  <Input type="email" className="shad-input" {...field} />
+                </FormControl>
+                <FormMessage />
+              </FormItem>
+            )}
+          />
+          <FormField
+            control={form.control}
+            name="password"
+            render={({ field }) => (
+              <FormItem>
+                <FormLabel>Password</FormLabel>
+                <FormControl>
+                  <Input type="password" className="shad-input" {...field} />
+                </FormControl>
+                <FormMessage />
+              </FormItem>
+            )}
+          />
+          <Button type="submit" className="shad-button_primary">Submit</Button>
+        </form>
+      </div>
+    </Form>
+  )
+}
+
+export default SignupForm
